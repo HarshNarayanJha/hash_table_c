b3097ebb5117baec3684c985368bd2e2b98a6570
Harsh Narayan Jha <harshnj@proton.me>
2025-04-30 18:18:17 +0530

    insert, search and delete entries

--- src/hash_table.h
+++ src/hash_table.h
@@ -25,4 +25,8 @@ ht_hash_table *ht_new(void);
 void ht_del_hash_table(ht_hash_table *ht);
 int ht_get_hash(const char *s, const int num_buckets, const int attempt);
 
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *ht, const char *key);
+
 #endif
